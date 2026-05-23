201ebd4a2d5fe0116457e1a68aed0e0c79ccc232
ShriyanshSingh0 <shriyansh691@gmail.com>
2026-05-23 14:57:50 +0530

    Adding the missing files

---
+++ Chapter_01_Variables-and-Datatypes/dataTypes.py
@@ -0,0 +1,21 @@
+#Examples of different data types in Python
+
+# String data type
+name = "Alice"
+print(type(name))  # Output: <class 'str'>
+
+# Integer data type
+age = 30
+print(type(age))  # Output: <class 'int'>
+
+# Float data type
+height = 1.75
+print(type(height))  # Output: <class 'float'>
+
+# Boolean data type
+is_student = True
+print(type(is_student))  # Output: <class 'bool'>
+
+# None data type
+favorite_color = None
+print(type(favorite_color))  # Output: <class 'NoneType'>
